9fac8fee1e9e506d816f97ea689cf0cd87fc30e9
Michelle Lee <michellechulee@gmail.com>
2021-06-26 16:15:02 -0700

    create Classic template

---
+++ src/templates/Classic.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+import {
+  TextInput,
+  SectionHeader,
+  Layout,
+  TextArea,
+  NavContents,
+  Nav,
+  ButtonRound,
+} from "components/index";
+
+
+export default function Classic(props) {
+
+  return (
+    <>
+        
+    </>
+  );
+}
