e343a31b5a56596c22a1bfe66a2d80e448af504f
Michelle Lee <michellechulee@gmail.com>
2021-06-25 18:41:36 -0700

    added redux template

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import {NavContents, Nav} from 'components/index';
 import {SectionBasic} from 'sections/index';
 
+import { Counter } from './features/counter/Counter';
+
+
 const Section = styled.div`
   display: flex;
   flex-direction: column;
@@ -21,7 +24,7 @@ export default function App() {
     <>
         {/* <Nav/> */}
         <NavContents/>
-
+        <Counter/>
         {/* <Section>
           <SectionBasic/>
         </Section> */}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
